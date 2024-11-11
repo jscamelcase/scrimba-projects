151693c715abb3435004ff4a78e10e7366e94631
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-11-11 17:13:14 +1100

    added delete all functionality

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -4,6 +4,11 @@ myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) myLeads = leadsFromLocalStorage;
+renderLeads();
 
 //this function renders the leads
 function renderLeads() {
@@ -18,14 +23,21 @@ function renderLeads() {
     const a = document.createElement("a");
     a.href = myLeads[i];
     a.target = "_blank";
-    a.textContent = myLeads[i]; //Sets the tect content, avoiding HTML parsing (symbols => to characters)
+    a.textContent = myLeads[i]; //Sets the tect content, avoiding HTML parsi ng (symbols => to characters)
     li.appendChild(a);
     ulEl.appendChild(li);
   }
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
