16d086ad15b607e94b07fd8902be1f35556f3fc7
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-11-14 11:49:46 +1100

    added conversion display

--- unit-conversion/script.js
+++ unit-conversion/script.js
@@ -5,17 +5,20 @@ const KILO_TO_POUND = 2.204;
 
 // DOM Objects
 const numberInput = document.querySelector("input");
+const btnConverter = document.getElementById("btn-convert");
 const lengthPara = document.getElementById("length");
 const volumePara = document.getElementById("volume");
 const weightPara = document.getElementById("weight");
 
 //Round down function
 function roundDownThreeDecimalPlaces(number) {
-  return Math.floor(number * 1000) / 1000;
+  return (Math.floor(number * 1000) / 1000).toFixed(3);
 }
 
-numberInput.addEventListener("input", (event) => {
-  const inputValue = Number(event.target.value);
+//Convert function
+
+function convertInput() {
+  const inputValue = numberInput.value;
   const metersToFeet = roundDownThreeDecimalPlaces(inputValue * METER_TO_FEET);
   const feetToMeters = roundDownThreeDecimalPlaces(
     (inputValue * 1) / METER_TO_FEET
@@ -31,5 +34,10 @@ numberInput.addEventListener("input", (event) => {
     (inputValue * 1) / KILO_TO_POUND
   );
 
-  console.log(metersToFeet);
-});
+  lengthPara.textContent = `${inputValue} meters = ${metersToFeet} feet | ${inputValue} feet = ${feetToMeters} meters`;
+  volumePara.textContent = `${inputValue} liters = ${litersToGallons} gallons | ${inputValue} gallons = ${gallonsToLiters}`;
+  weightPara.textContent = `${inputValue} kilos = ${kilosToPounds} | ${inputValue} pounds = ${poundsToKilos} kilos`;
+}
+
+// numberInput.addEventListener("input", convertInput);
+btnConverter.addEventListener("click", convertInput);
